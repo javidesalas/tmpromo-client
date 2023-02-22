3e71b0da8fcf764d241551e33be1abd70797c4d3
Javier de Salas <javidesalas@gmail.com>
2023-02-22 18:53:57 +0100

    fix Procfile

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,19 +1,29 @@
-// Create new file scripts/heroku-start.js
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Check if request is from HTTP and if so, redirect to HTTPS
+app.enable("trust proxy");
+app.use((req, res, next) => {
+	req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
+});
+
 app.use(express.json());
-// Your static pre-build assets folder
+
+// Your static site folder name
 app.use(express.static(path.join(__dirname, "..", "build")));
-// Root Redirects to the pre-build assets
+
+// Root Redirects to the build in assets folder
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "..", "build"));
 });
-// Any Page Redirects to the pre-build assets folder index.html that // will load the react app
+
+// Any Page Redirects to the build in assets folder index.html that will load the react app
 app.get("*", function (req, res) {
 	res.sendFile(path.join(__dirname, "..", "build/index.html"));
 });
+
 app.listen(port, () => {
 	console.log("Server is running on port: ", port);
 });
